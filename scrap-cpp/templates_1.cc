c7299bf7714fcaa6e06f4021c4801438bf3207b0
Benjamin Chang <benjaminchang23@gmail.com>
2021-04-25 22:43:58 -0400

    feat: add some basic template scrap for warmup

---
+++ scrap-cpp/templates_1.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+template <typename T>
+T SimpleSum(T a, T b)
+{
+    return a + b;
+}
+
+template <typename T>
+T SimpleMultiply(T a, T b)
+{
+    return a * b;
+}
+
+template <typename T, int I>
+T FixedMultiply(T num)
+{
+    return num * I;
+}
+
+int main () {
+
+    int i = 5;
+    int j = 10;
+    double a = 5.0;
+    double b = 11.3;
+    std::cout << SimpleSum(i, j) << std::endl;
+    std::cout << SimpleSum(a, b) << std::endl;
+
+    std::cout << SimpleMultiply(i, j) << std::endl;
+    std::cout << SimpleMultiply(a, b) << std::endl;
+
+    std::cout << FixedMultiply<int, 3>(10) << std::endl;
+
+    return 0;
+}
