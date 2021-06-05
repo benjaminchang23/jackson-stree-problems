3c4dc77176114bd414c219f328a0733e2b9194c8
Benjamin Chang <benjaminchang23@gmail.com>
2021-06-05 14:48:14 -0400

    feat: imgui adapter library building

--- dear-imgui/dear_imgui_0.cc
+++ dear-imgui/dear_imgui_0.cc
@@ -1,8 +1,14 @@
-#include <imgui.h>
-#include <backends/imgui_impl_glfw.h>
-#include <backends/imgui_impl_opengl3.h>
+// #include <imgui.h>
+// #include <backends/imgui_impl_glfw.h>
+// #include <backends/imgui_impl_opengl3.h>
+
+#include <GL/gl3w.h> 
+
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    ImGui::ShowDemoWindow();
+    // ImGui::ShowDemoWindow();
+
+    return 0;
 }
