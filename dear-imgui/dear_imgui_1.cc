941ada6b90d94bdd750ecafa01312dc0c71db642
Benjamin Chang <benjaminchang23@gmail.com>
2021-06-11 22:46:25 -0400

    feat: update demo with more tables, inputs, and buttons

--- dear-imgui/dear_imgui_1.cc
+++ dear-imgui/dear_imgui_1.cc
@@ -57,7 +57,9 @@ using namespace gl;
 #endif
 
 static void ShowAppMainMenuBar();
-static void ShowWindowTables();
+static void ShowRefreshAndSearch();
+static void ShowFirstTable();
+static void ShowSecondTable();
 
 static void HelpMarker(const char* desc)
 {
@@ -205,7 +207,11 @@ int main(int, char**)
         ImGui::PushItemWidth(ImGui::GetFontSize() * -12);
 
         ShowAppMainMenuBar();
-        ShowWindowTables();
+        ShowRefreshAndSearch();
+        ImGui::Spacing();
+        ShowFirstTable();
+        ImGui::Spacing();
+        ShowSecondTable();
 
         ImGui::PopItemWidth();
         ImGui::End();
@@ -332,7 +338,33 @@ static void PopStyleCompact()
     ImGui::PopStyleVar(2);
 }
 
-static void ShowWindowTables()
+static void ShowRefreshAndSearch()
+{
+    static int refresh_clicked = 0;
+    if (ImGui::Button("Refresh"))
+        refresh_clicked++;
+    if (refresh_clicked & 1)
+    {
+        ImGui::SameLine();
+        ImGui::Text("Refreshing...");
+    }
+
+    ImGui::SameLine();
+    static char str1[128] = "";
+    ImGui::InputTextWithHint("", "Search", str1, IM_ARRAYSIZE(str1));
+    ImGui::SameLine();
+
+    static int search_clicked = 0;
+    if (ImGui::Button("Search"))
+        search_clicked++;
+    if (search_clicked & 1)
+    {
+        ImGui::SameLine();
+        ImGui::Text("Searching...");
+    }
+}
+
+static void ShowFirstTable()
 {
     // const float TEXT_BASE_WIDTH = ImGui::CalcTextSize("A").x;
     const float TEXT_BASE_HEIGHT = ImGui::GetTextLineHeightWithSpacing();
@@ -346,8 +378,96 @@ static void ShowWindowTables()
         "Kiwi", "Orange", "Pineapple", "Blueberry", "Plum", "Coconut", "Pear", "Apricot"
     };
 
-    if (ImGui::TreeNode("Sorting"))
+    // Create item list
+    static ImVector<MyItem> items;
+    if (items.Size == 0)
     {
+        items.resize(50, MyItem());
+        for (int n = 0; n < items.Size; n++)
+        {
+            const int template_n = n % IM_ARRAYSIZE(template_items_names);
+            MyItem& item = items[n];
+            item.ID = n;
+            item.Name = template_items_names[template_n];
+            item.Quantity = (n * n - n) % 20; // Assign default quantities
+        }
+    }
+
+    // Options
+    static ImGuiTableFlags flags =
+        ImGuiTableFlags_Resizable | ImGuiTableFlags_Reorderable | ImGuiTableFlags_Hideable | ImGuiTableFlags_Sortable | ImGuiTableFlags_SortMulti
+        | ImGuiTableFlags_RowBg | ImGuiTableFlags_BordersOuter | ImGuiTableFlags_BordersV | ImGuiTableFlags_NoBordersInBody
+        | ImGuiTableFlags_ScrollY;
+    PushStyleCompact();
+    ImGui::SameLine(); HelpMarker("When sorting is enabled: hold shift when clicking headers to sort on multiple column. TableGetSortSpecs() may return specs where (SpecsCount > 1).");
+    ImGui::SameLine(); HelpMarker("When sorting is enabled: allow no sorting, disable default sorting. TableGetSortSpecs() may return specs where (SpecsCount == 0).");
+    PopStyleCompact();
+
+    ImGui::Text("First Table");
+
+    if (ImGui::BeginTable("table_sorting", 4, flags, ImVec2(0.0f, TEXT_BASE_HEIGHT * 30), 0.0f))
+    {
+        // Declare columns
+        // We use the "user_id" parameter of TableSetupColumn() to specify a user id that will be stored in the sort specifications.
+        // This is so our sort function can identify a column given our own identifier. We could also identify them based on their index!
+        // Demonstrate using a mixture of flags among available sort-related flags:
+        // - ImGuiTableColumnFlags_DefaultSort
+        // - ImGuiTableColumnFlags_NoSort / ImGuiTableColumnFlags_NoSortAscending / ImGuiTableColumnFlags_NoSortDescending
+        // - ImGuiTableColumnFlags_PreferSortAscending / ImGuiTableColumnFlags_PreferSortDescending
+        ImGui::TableSetupColumn("ID",       ImGuiTableColumnFlags_DefaultSort          | ImGuiTableColumnFlags_WidthFixed,   0.0f, MyItemColumnID_ID);
+        ImGui::TableSetupColumn("Name",                                                  ImGuiTableColumnFlags_WidthFixed,   0.0f, MyItemColumnID_Name);
+        ImGui::TableSetupColumn("Action",   ImGuiTableColumnFlags_NoSort               | ImGuiTableColumnFlags_WidthFixed,   0.0f, MyItemColumnID_Action);
+        ImGui::TableSetupColumn("Quantity", ImGuiTableColumnFlags_PreferSortDescending | ImGuiTableColumnFlags_WidthStretch, 0.0f, MyItemColumnID_Quantity);
+        ImGui::TableSetupScrollFreeze(0, 1); // Make row always visible
+        ImGui::TableHeadersRow();
+
+        // Sort our data if sort specs have been changed!
+        if (ImGuiTableSortSpecs* sorts_specs = ImGui::TableGetSortSpecs())
+            if (sorts_specs->SpecsDirty)
+            {
+                MyItem::s_current_sort_specs = sorts_specs; // Store in variable accessible by the sort function.
+                if (items.Size > 1)
+                    qsort(&items[0], (size_t)items.Size, sizeof(items[0]), MyItem::CompareWithSortSpecs);
+                MyItem::s_current_sort_specs = NULL;
+                sorts_specs->SpecsDirty = false;
+            }
+
+        // Demonstrate using clipper for large vertical lists
+        ImGuiListClipper clipper;
+        clipper.Begin(items.Size);
+        while (clipper.Step())
+            for (int row_n = clipper.DisplayStart; row_n < clipper.DisplayEnd; row_n++)
+            {
+                // Display a data item
+                MyItem* item = &items[row_n];
+                ImGui::PushID(item->ID);
+                ImGui::TableNextRow();
+                ImGui::TableNextColumn();
+                ImGui::Text("%04d", item->ID);
+                ImGui::TableNextColumn();
+                ImGui::TextUnformatted(item->Name);
+                ImGui::TableNextColumn();
+                ImGui::SmallButton("None");
+                ImGui::TableNextColumn();
+                ImGui::Text("%d", item->Quantity);
+                ImGui::PopID();
+            }
+        ImGui::EndTable();
+    }
+}
+
+static void ShowSecondTable()
+{
+    if (ImGui::TreeNode("Second Table"))
+    {
+        const float TEXT_BASE_HEIGHT = ImGui::GetTextLineHeightWithSpacing();
+
+        static const char* template_items_names[] =
+        {
+            "Banana", "Apple", "Cherry", "Watermelon", "Grapefruit", "Strawberry", "Mango",
+            "Kiwi", "Orange", "Pineapple", "Blueberry", "Plum", "Coconut", "Pear", "Apricot"
+        };
+
         // Create item list
         static ImVector<MyItem> items;
         if (items.Size == 0)
@@ -368,14 +488,8 @@ static void ShowWindowTables()
             ImGuiTableFlags_Resizable | ImGuiTableFlags_Reorderable | ImGuiTableFlags_Hideable | ImGuiTableFlags_Sortable | ImGuiTableFlags_SortMulti
             | ImGuiTableFlags_RowBg | ImGuiTableFlags_BordersOuter | ImGuiTableFlags_BordersV | ImGuiTableFlags_NoBordersInBody
             | ImGuiTableFlags_ScrollY;
-        PushStyleCompact();
-        ImGui::CheckboxFlags("ImGuiTableFlags_SortMulti", &flags, ImGuiTableFlags_SortMulti);
-        ImGui::SameLine(); HelpMarker("When sorting is enabled: hold shift when clicking headers to sort on multiple column. TableGetSortSpecs() may return specs where (SpecsCount > 1).");
-        ImGui::CheckboxFlags("ImGuiTableFlags_SortTristate", &flags, ImGuiTableFlags_SortTristate);
-        ImGui::SameLine(); HelpMarker("When sorting is enabled: allow no sorting, disable default sorting. TableGetSortSpecs() may return specs where (SpecsCount == 0).");
-        PopStyleCompact();
 
-        if (ImGui::BeginTable("table_sorting", 4, flags, ImVec2(0.0f, TEXT_BASE_HEIGHT * 15), 0.0f))
+        if (ImGui::BeginTable("table_sorting", 4, flags, ImVec2(0.0f, TEXT_BASE_HEIGHT * 30), 0.0f))
         {
             // Declare columns
             // We use the "user_id" parameter of TableSetupColumn() to specify a user id that will be stored in the sort specifications.
