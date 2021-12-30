f47abf33866ce199104d4d0f46529405c4820eae
Benjamin Chang <benjaminchang23@gmail.com>
2021-12-30 02:21:32 -0500

    feat: add more spdlog examples

--- spdlog/spdlog_4.cc
+++ spdlog/spdlog_4.cc
@@ -7,7 +7,7 @@ int main()
     auto max_size = 1048576 * 5;
     auto max_files = 3;
     const auto logger_name = "some_logger_name";
-    const auto log_name = "rotating.txt";
+    const auto log_name = "logs/rotating.txt";
     auto logger = spdlog::rotating_logger_mt(logger_name, log_name, max_size, max_files);
     logger->info("Start logger with name {} at {}", logger_name, log_name);
     spdlog::get("some_logger_name")->info("loggers can be retrieved from a global registry using the spdlog::get(logger_name)");
