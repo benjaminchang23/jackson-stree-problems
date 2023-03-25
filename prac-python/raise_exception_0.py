225e7883762527012e6e2175fe43e25fe4bd6f46
Benjamin Chang <benjaminchang23@gmail.com>
2023-03-25 10:52:04 -0400

    feat: update prac

--- prac-python/raise_exception_0.py
+++ prac-python/raise_exception_0.py
@@ -1,6 +1,17 @@
 paths = []
 result = {}
 
-contents = result.get("contents")
-if not contents:
-    raise Exception("exception raised!")
+try:
+    contents = result.get("contents")
+    if not contents:
+        # AssertionError would be best prac
+        raise Exception("no contents", contents)
+except ValueError as error:
+    print(error.args)
+    raise
+except AssertionError as error:
+    print(error.args)
+    raise
+except Exception as error:
+    print(error.args)
+    raise
