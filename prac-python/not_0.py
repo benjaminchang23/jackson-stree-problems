f48c6880c759656eb1d0a057b9643d5be6916271
Benjamin Chang <benjaminchang23@gmail.com>
2023-12-30 13:54:30 -0500

    feat: prototype for logic

--- prac-python/not_0.py
+++ prac-python/not_0.py
@@ -1,11 +1,19 @@
 if not (True or True):
-    print("true" "true")
+    print("true true")
 
 if not (True or False):
-    print("true" "true")
+    print("true true")
 
 if not (False or True):
-    print("true" "true")
+    print("true true")
 
 if not (False or False):
-    print("false" "false")
+    print("false false")
+
+
+list_0 = ["a", "bcd", "def"]
+
+for ele in list_0:
+    if not("a" in ele or "b" in ele):
+        print("not match: {}".format(ele))
+        continue
