3e4dc8804d35dc6662a6791c0da7912ad28d3766
Benjamin Chang <benjaminchang23@gmail.com>
2024-01-09 22:29:02 -0500

    feat: add to not prac

--- prac-python/not_0.py
+++ prac-python/not_0.py
@@ -11,9 +11,11 @@ if not (False or False):
     print("false false")
 
 
-list_0 = ["a", "bcd", "def"]
+list_0 = ["a", "bd", "cef", "goov"]
 
 for ele in list_0:
-    if not("a" in ele or "b" in ele):
+    if not("a" in ele or "b" in ele or "c" in ele):
         print("not match: {}".format(ele))
         continue
+    print("match: {}".format(ele))
+    
