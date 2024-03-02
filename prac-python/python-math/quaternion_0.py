a18f37f903e5509b9e4e9319ea05faa3033bf948
Benjamin Chang <benjaminchang23@gmail.com>
2024-03-01 21:25:06 -0500

    feat: update example

--- prac-python/python-math/quaternion_0.py
+++ prac-python/python-math/quaternion_0.py
@@ -1,5 +1,8 @@
 # from: https://stackoverflow.com/questions/4870393/rotating-coordinate-system-via-a-quaternion
 
+import math
+import numpy
+
 def q_mult(q1, q2):
     w1, x1, y1, z1 = q1
     w2, x2, y2, z2 = q2
@@ -17,7 +20,42 @@ def qv_mult(q1, v1):
     q2 = (0.0,) + v1
     return q_mult(q_mult(q1, q2), q_conjugate(q1))[1:]
 
+def normalize(v, tolerance=0.00001):
+    mag2 = sum(n * n for n in v)
+    if abs(mag2 - 1.0) > tolerance:
+        mag = math.sqrt(mag2)
+        v = tuple(n / mag for n in v)
+    return v
+
+def axisangle_to_q(v, theta):
+    v = normalize(v)
+    x, y, z = v
+    theta /= 2
+    w = math.cos(theta)
+    x = x * math.sin(theta)
+    y = y * math.sin(theta)
+    z = z * math.sin(theta)
+    return w, x, y, z
+
+def q_to_axisangle(q):
+    w, v = q[0], q[1:]
+    theta = math.acos(w) * 2.0
+    return normalize(v), theta
+
 q1 = -0.007, -0.015, -0.432, 0.902
 v1 = -675, 423.318, 17.850
 
 print(qv_mult(q1, v1))
+
+x_axis_unit = (1, 0, 0)
+y_axis_unit = (0, 1, 0)
+z_axis_unit = (0, 0, 1)
+r1 = axisangle_to_q(x_axis_unit, numpy.pi / 2)
+r2 = axisangle_to_q(y_axis_unit, numpy.pi / 2)
+r3 = axisangle_to_q(z_axis_unit, numpy.pi / 2)
+
+v = qv_mult(r1, y_axis_unit)
+v = qv_mult(r2, v)
+v = qv_mult(r3, v)
+
+print(v)
