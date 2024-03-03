66303840c268bc91074516cb95a1d37a70482d04
Benjamin Chang <benjaminchang23@gmail.com>
2024-03-02 19:55:47 -0500

    feat: add to quat math

--- prac-python/python-math/quaternion_0.py
+++ prac-python/python-math/quaternion_0.py
@@ -47,6 +47,7 @@ v1 = -675, 423.318, 17.850
 
 print(qv_mult(q1, v1))
 
+# A sequence of 90-degree rotations about the x, y, and z axes will return a vector on the y axis to its original position.
 x_axis_unit = (1, 0, 0)
 y_axis_unit = (0, 1, 0)
 z_axis_unit = (0, 0, 1)
@@ -54,8 +55,37 @@ r1 = axisangle_to_q(x_axis_unit, numpy.pi / 2)
 r2 = axisangle_to_q(y_axis_unit, numpy.pi / 2)
 r3 = axisangle_to_q(z_axis_unit, numpy.pi / 2)
 
-v = qv_mult(r1, y_axis_unit)
-v = qv_mult(r2, v)
-v = qv_mult(r3, v)
+print(f"r1: {r1}")
+print(f"r2: {r2}")
+print(f"r3: {r3}")
 
+v = qv_mult(r1, (0, 1, 0,))
 print(v)
+
+v = qv_mult(r2, v)
+print(v)
+
+v = qv_mult(r3, v)
+print(v)
+
+assert int(v[0]) == 0
+assert int(v[1]) == 1
+assert int(v[2]) == 0
+
+# A sequence of 90-degree rotations about the y, x, and z axes will return a vector on the x axis to its original position.
+r1 = axisangle_to_q(x_axis_unit, numpy.pi / 2)
+r2 = axisangle_to_q(y_axis_unit, numpy.pi / 2)
+r3 = axisangle_to_q(z_axis_unit, -numpy.pi / 2)
+
+v = qv_mult(r2, (1, 0, 0,))
+print(v)
+
+v = qv_mult(r1, v)
+print(v)
+
+v = qv_mult(r3, v)
+print(v)
+
+assert int(v[0]) == 1
+assert int(v[1]) == 0
+assert int(v[2]) == 0
