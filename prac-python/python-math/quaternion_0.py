8c3175126a1ffc5c2ac73f5fe15ec5bd100b2acf
Benjamin Chang <benjaminchang23@gmail.com>
2024-03-04 20:45:05 -0500

    feat: update quat ordering to xyzw

--- prac-python/python-math/quaternion_0.py
+++ prac-python/python-math/quaternion_0.py
@@ -4,21 +4,22 @@ import math
 import numpy
 
 def q_mult(q1, q2):
-    w1, x1, y1, z1 = q1
-    w2, x2, y2, z2 = q2
-    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+    x1, y1, z1, w1 = q1
+    x2, y2, z2, w2 = q2
     x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
     y = w1 * y2 + y1 * w2 + z1 * x2 - x1 * z2
     z = w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2
-    return w, x, y, z
+    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+
+    return x, y, z, w
 
 def q_conjugate(q):
-    w, x, y, z = q
-    return (w, -x, -y, -z)
+    x, y, z, w = q
+    return (-x, -y, -z, w)
 
 def qv_mult(q1, v1):
-    q2 = (0.0,) + v1
-    return q_mult(q_mult(q1, q2), q_conjugate(q1))[1:]
+    q2 = v1 + (0.0,)
+    return q_mult(q_mult(q1, q2), q_conjugate(q1))[:-1]
 
 def normalize(v, tolerance=0.00001):
     mag2 = sum(n * n for n in v)
@@ -35,10 +36,10 @@ def axisangle_to_q(v, theta):
     x = x * math.sin(theta)
     y = y * math.sin(theta)
     z = z * math.sin(theta)
-    return w, x, y, z
+    return x, y, z, w
 
 def q_to_axisangle(q):
-    w, v = q[0], q[1:]
+    v, w = q[:-1], q[3]
     theta = math.acos(w) * 2.0
     return normalize(v), theta
 
@@ -74,6 +75,10 @@ assert int(v[2]) == 0
 r1 = axisangle_to_q(x_axis_unit, numpy.pi / 2)
 r2 = axisangle_to_q(y_axis_unit, numpy.pi / 2)
 r3 = axisangle_to_q(z_axis_unit, -numpy.pi / 2)
+
+print(f"r1: {r1}")
+print(f"r2: {r2}")
+print(f"r3: {r3}")
 
 v = qv_mult(r2, (1, 0, 0,))
 print(v)
