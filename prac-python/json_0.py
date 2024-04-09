d3ba0b58df4eb71a5be7cc6baa8583015d4b250a
Benjamin Chang <benjaminchang23@gmail.com>
2024-04-09 17:37:11 -0400

    feat: add null to json prac

--- prac-python/json_0.py
+++ prac-python/json_0.py
@@ -3,11 +3,15 @@ import json
 dict_0 = {
     "clear": True,
 }
-
 print(dict_0)
-
 print('{"clear": true}')
+loaded_json_0 = json.loads('{"clear": true}')
+print(loaded_json_0)
 
-loaded_json = json.loads('{"clear": true}')
-
-print(loaded_json)
+dict_1 = {
+    "clear": None,
+}
+print(dict_1)
+print('{"clear": null}')
+loaded_json_1 = json.loads('{"clear": null}')
+print(loaded_json_1)
