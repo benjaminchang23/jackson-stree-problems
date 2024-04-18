e885e9d00751d309724229f2f39a5852c0a657c8
Benjamin Chang <benjaminchang23@gmail.com>
2024-04-17 22:41:47 -0400

    feat: start decorator prac

---
+++ prac-python/bool_0.py
@@ -0,0 +1,6 @@
+not_a_bool: bool = "foo"
+
+if not_a_bool:
+    print(not_a_bool)
+else:
+    print("hello")
---
+++ prac-python/decorator_0.py
@@ -0,0 +1,19 @@
+def inner_dec(func):
+    def inner_func(*args, **kwargs):
+        print("inner dec before")
+        func(*args, **kwargs)
+        print("inner dec after")
+    return inner_func(func)
+
+
+def outer_dec(func):
+    def inner_func(*args, **kwargs):
+        print("outer dec before")
+        func(*args, **kwargs)
+        print("outer dec after")
+    return inner_func(func)
+
+@inner_dec
+@outer_dec
+def print_hello(print_str: str):
+    print(print_str)
