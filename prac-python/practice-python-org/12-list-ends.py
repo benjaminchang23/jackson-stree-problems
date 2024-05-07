f12f232edabc1ed4faa6e3fea5e068aff5998e1f
Benjamin Chang <benjaminchang23@gmail.com>
2024-05-07 18:48:01 -0400

    fix: add source

--- prac-python/practice-python-org/12-list-ends.py
+++ prac-python/practice-python-org/12-list-ends.py
@@ -1,3 +1,6 @@
+# https://www.practicepython.org/exercise/2014/04/25/12-list-ends.html
+# Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
+
 from typing import List
 
 def first_and_last(input_list: List):
