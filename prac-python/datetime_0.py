412cb8dc0e8a3218e92c98963e84fc3d1a8caabe
Benjamin Chang <benjaminchang23@gmail.com>
2024-05-17 21:16:58 -0400

    feat: datetime

--- prac-python/datetime_0.py
+++ prac-python/datetime_0.py
@@ -3,3 +3,21 @@ import datetime
 # get date string
 today = datetime.date.today()
 print(today)
+
+def daily_task():
+    today = datetime.date.today()
+    print(today.weekday())
+    if today.weekday() in [1, 2]: # 1 corresponds to Tuesday, 2 corresponds to Wednesday
+        print("correct weekday")
+        print(today.month)
+        if today.month not in [1, 2, 3, 12]: # no street sweeping in the winter
+            print("correct month")
+            week_of_month = (today.day - 1) // 7 + 1
+            if week_of_month == 3:
+                user_ids = ["148906826790993920", "694758082479128637"]
+                mentions = ""
+                for user_id in user_ids:
+                    mentions += "<@" + user_id + "> "
+                print("Street cleanup is tomorrow!")
+
+daily_task()
